999c0479e3727256e4068c21267bf862c4a5ddc7
Zakariakarboub <49561317+Zakariakarboub@users.noreply.github.com>
2019-10-24 20:18:16 +0200

    tussentijdse commit

--- Goudkoorts_Code/Domain/Cart.cs
+++ Goudkoorts_Code/Domain/Cart.cs
@@ -6,5 +6,27 @@ namespace Goudkoorts_Code.Domain
 {
     class Cart : Vehicle
     {
+        public Cart(BaseRail baseRail)
+        {
+            onTrack = baseRail;
+            Load = 1;
+        }
+
+        public override bool Move()
+        {
+            if (onTrack.Next == null || onTrack.Next.MoveToThis(this))
+            {
+                return true;
+            }
+            return false;
+        }
+        public override char Print()
+        {
+            if (Load == 0)
+            {
+                return 'o';
+            }
+            return 'ø';
+        }
     }
 }
