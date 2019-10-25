2f4679159d35c955f91ed929120f45ab25d8ca4a
TmKrts <tkaerts@avans.nl>
2019-10-25 16:05:34 +0200

    Betere beginsituatie

--- Goudkoorts_Code/View/OutputView.cs
+++ Goudkoorts_Code/View/OutputView.cs
@@ -13,11 +13,11 @@ namespace Goudkoorts_Code.View
             Console.Clear();
             Console.WriteLine("Hello and welcome to goudkoort");
             Console.WriteLine("It is your task to give the miningcart directions to the dock by switching the switches:");
-            Console.WriteLine("The red switch can be changed by pressing 1");
-            Console.WriteLine("The red switch can be changed by pressing 2");
-            Console.WriteLine("The red switch can be changed by pressing 3");
-            Console.WriteLine("The red switch can be changed by pressing 4");
-            Console.WriteLine("The red switch can be changed by pressing 5");
+            Console.WriteLine("The first switch can be changed by pressing 1");
+            Console.WriteLine("The second switch can be changed by pressing 2");
+            Console.WriteLine("The third switch can be changed by pressing 3");
+            Console.WriteLine("The fourth switch can be changed by pressing 4");
+            Console.WriteLine("The fifth switch can be changed by pressing 5");
             Console.WriteLine();
             Console.WriteLine("> press any key to continue");
             input = Console.ReadKey();
